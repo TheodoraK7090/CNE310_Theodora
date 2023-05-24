2a33c730a6d53e58639b1b55844731524c369c1d
adamjohnson650 <133832291+adamjohnson650@users.noreply.github.com>
2023-05-23 19:22:27 -0700

    Code is bettert
    Imade a few upgrades still need to fix minor issues

--- debug2.py
+++ debug2.py
@@ -1,20 +1,24 @@
-def score_to_letter_grade(grade):
-if grade > 90:
-return "A"
-elif grade >= 87:
-return "B+"
-elif grade == 80:
-return "B"
-elif grade >= 77:
-return "C+"
-elif grade <= 70:
-return "C"
-elif grade >= 67:
-return "D+"
-elif grade >= 60:
-return "D"
-else:
-return "F"
+number_grade = int(input("Enter how many grades are you entering here:"))
+l=[]
+for i in range(number_grade):
+    g1=int(input("Enter grade #"+str(i+1)+": "))
+    l.append(g1)
+student=(sum(l)/(len(l)*100))*100
+letter_grade=""
+if student > 93:
+    letter_grade = "A"
+    print("You Got an A")
+elif student > 79 and student <= 92.99:
+    letter_grade="B"
+elif student > 73 and student  <= 78.99:
+    letter_grade="C"
+elif student > 61.99 and student <= 72.99:
+    letter_grade="D"
+elif student < 61.99:
+    letter_grade = "F"
+if letter_grade != "A":
+    print("not an A")
+
 print("Grade of 90 should be A: " + score_to_letter_grade(90))
 print("Grade of 87 should be B+: " + score_to_letter_grade(87))
 print("Grade of 81 should be B: " + score_to_letter_grade(81))
